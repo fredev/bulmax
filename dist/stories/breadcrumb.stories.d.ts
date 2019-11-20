e384fa15f42b4d6431653d6014b61f522e2eb7c2
fredev <feligida63@gmail.com>
2019-11-20 19:27:31 +0100

    remove stories from dist folder

--- dist/stories/breadcrumb.stories.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare const _default: {
-    title: string;
-};
-export default _default;
-export declare const SimpleBreadcrumb: () => JSX.Element;
-export declare const Separators: () => JSX.Element;
-export declare const Sizes: () => JSX.Element;
--- dist/stories/index.stories.js
+++
@@ -1,3 +0,0 @@
-import 'bulma/css/bulma.min.css';
-import './style.css';
-//# sourceMappingURL=index.stories.js.map
