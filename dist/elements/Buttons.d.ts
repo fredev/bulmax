03c07d4b52ff357083ae0efc0814b03d119cb6db
fredev <feligida63@gmail.com>
2019-11-24 09:33:32 +0100

    build 1.0.0-10

--- dist/elements/Buttons.d.ts
+++ dist/elements/Buttons.d.ts
@@ -1,12 +1,12 @@
 import React, { ReactElement } from "react";
 interface ButtonsProps {
     children: ReactElement | ReactElement[];
-    small: boolean;
-    medium: boolean;
-    large: boolean;
-    addons: boolean;
-    centered: boolean;
-    right: boolean;
+    small?: boolean;
+    medium?: boolean;
+    large?: boolean;
+    addons?: boolean;
+    centered?: boolean;
+    right?: boolean;
 }
 declare const Buttons: React.FC<ButtonsProps>;
 export default Buttons;
